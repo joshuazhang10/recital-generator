6abcbb41cb96ceb7a3e2b16c283b8ccd7932cf87
Joshua Zhang <122920988+joshuazhang10@users.noreply.github.com>
2025-10-26 14:45:38 -0400

    Added .env for api base url, small ui changes (#2)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,33 @@
 import Navbar from './components/Navbar'
 import PieceCard from "@/components/PieceCard"
 import { Input } from "@/components/ui/input"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
 
-  return (
-    <div>
-      <Navbar />
-      <Input type="search" placeholder="Search..."/>
-      <div className="flex gap-4 min-h-svh flex-row items-center justify-center">
-        <PieceCard title="Piece 1"/>
-        <PieceCard title="Piece 750"/>
-        <PieceCard title="Test Piece"/>
-      </div>
+    useEffect(() => {
+        fetch('')
+    }, []);
+
+    return (
+        <div>
+        <header className="inline-flex">
+            <Navbar />
+            <Avatar>
+            <AvatarImage />
+            <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+        </header>
+        <Input type="search" placeholder="Search..."/>
+        <div className="flex gap-4 min-h-svh flex-row items-center justify-center">
+            <PieceCard title="Piece 1"/>
+            <PieceCard title="Piece 750"/>
+            <PieceCard title="Test Piece"/>
+        </div>
     </div>
-  );
+    );
 }
 
 export default App;
